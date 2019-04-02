07653382b1737e1d80cc20215f0ff591621abe64
Pedro Henrique Carvalho <peedro.gc@gmail.com>
2019-04-01 23:45:22 -0300

    Tradução do Spec para pt/br

--- jasmine/spec/validadorSpec.js
+++ jasmine/spec/validadorSpec.js
@@ -1,35 +1,35 @@
-describe("Transform String to Array", function() {
+describe("Tranforma String em Array", function() {
     beforeEach(function() {
         cpf = "12345678909"
     })
-    it("Should be able to transform String to Array", function() {
+    it("Deve ser capaz de transformar String em Array", function() {
         expect(validador.main()).toEqual(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '9'])
     })
 })
-describe("Validate this CPF: 123 456 789 - 09", function () {
+describe("Validar esse CPF: 123 456 789 - 09", function () {
     beforeEach(function () {
         cpfArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 9];
     })
-    it("Should be able to calculate a Array", function () {
+    it("Deve ser capaz de calcular um Array", function () {
         expect(validador.calc()).toEqual([10, 9]);
     })
 })
-describe("Verify if is true or false", function () {
+describe("Verifica se é Verdadeiro ou Falso", function () {
     beforeEach(function () {
         cpfArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 9];  
         soma1 = 10;
         soma2 = 9;
     })
-    it("Should be true when soma1 = 10", function () {
+    it("Deve ser verdadeiro quando soma1 = 10", function () {
         expect(validador.ver1()).toBeTruthy();
     })
-    it("Should be false when soma1 = 10", function () {
+    it("Deve ser falso quando soma1 = 10", function () {
         expect(validador.ver2()).not.toBeTruthy();
     })
-    it("Should be false when soma2 = 9", function () {
+    it("Deve ser falso quando soma2 = 9", function () {
         expect(validador.ver3()).not.toBeTruthy();
     })
-    it("Should be true when soma2 = 9", function () {
+    it("Deve ser verdadeiro quando soma2 = 9", function () {
         expect(validador.ver4()).toBeTruthy();
     })
 })
